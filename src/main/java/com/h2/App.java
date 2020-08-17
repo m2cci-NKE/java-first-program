613b7d1e850a50fd85893fae52dde789aa1cd75b
m2cci <nour_87@hotmail.fr>
2020-08-17 11:53:14 +0200

    projetc completed

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -6,12 +6,18 @@ package com.h2;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+   
+	
+	
+	public static int doubleTheNumber(int number) {
+        return 2*number;
     }
-
-    public static int doubleTheNumber(int number) {
-        return -1;
+    
+    
+    private static int add( int[] numbers) {
+    	
+    	int sum=0;
+    	for (int number : numbers) { sum+= number;}
+    	return sum;
     }
 }
---
+++ src/main/java/com/h2/BestLoanRates.java
@@ -0,0 +1,40 @@
+package com.h2;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class BestLoanRates {
+	
+	public static final Map<Integer,Float>bestRates= Map.of(1, 5.50f, 2, 3.45f, 3, 2.67f);
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner scanner= new Scanner(System.in);
+		
+		System.out.println("Enter your name ");
+		
+		String name = scanner.nextLine();
+		
+		System.out.println("Hello " + name);
+		System.out.println("Enter the loan term (in years)");
+		int loanTermInYears = scanner.nextInt();
+		float bestRates= getRates(loanTermInYears);
+		
+		if (bestRates == 0.0f) 
+			System.out.println("No available rates for term: " + loanTermInYears + " years");
+		else
+			System.out.println("Best Available Rate: " + getRates(loanTermInYears) + "%");
+		
+		scanner.close();
+		
+	}
+
+	public static float getRates(int leanTermYears) {
+		
+		if (bestRates.containsKey(leanTermYears)) 
+			return bestRates.get(leanTermYears);
+		return 0.0f;
+	}
+}
